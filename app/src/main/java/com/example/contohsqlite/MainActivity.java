7118f989b91958dca05b5e90c427622513e33cd0
cassie <cassiejc123@gmail.com>
2023-09-21 12:09:00 +0700

    update main activity

--- app/src/main/java/com/example/contohsqlite/MainActivity.java
+++ app/src/main/java/com/example/contohsqlite/MainActivity.java
@@ -16,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
         dbHandler dbhandler = new dbHandler(this);
 
-//        dbhandler.addContact(new Contact("Budi", "080000000000"));
-//        dbhandler.addContact(new Contact("Siti", "081111111111"));
-//        dbhandler.addContact(new Contact("Tono", "082222222222"));
+        dbhandler.addContact(new Contact("Budi", "080000000000"));
+        dbhandler.addContact(new Contact("Siti", "081111111111"));
+        dbhandler.addContact(new Contact("Tono", "082222222222"));
 
         Log.d("SQLITE", "Membaca isi Database");
         List<Contact> contacts = dbhandler.getAllContacts();
         for (Contact c: contacts){
-            String m = "id: "+c.getId()+ "Nama: "+c.getName()+ " Phone: "+c.getPhone();
-            Log.d("SQLITE", m);
+            String m = "id: "+c.getId()+ "Nama: "+c.getName()+ "Phone: "+c.getPhone();
+            Log.d("SQLITE",m);
         }
 
     }
